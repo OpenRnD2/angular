ff00f43bb6c2f212ae5350b31b46fadc61a21270
Mathieu Renda <mathieu.renda@gmail.com>
2016-02-29 13:27:07 +0100

    Added the module definition

---
+++ src/OpenRnD.AppNgine/Module.ts
@@ -0,0 +1,11 @@
+﻿
+module appNgine {
+
+    var mod = angular.module('appNgine', []);
+
+    // TODO: Register components here
+
+    export function module(): ng.IModule {
+        return mod;
+    }
+}
